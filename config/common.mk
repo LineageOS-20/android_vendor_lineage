882982ad02460c4868fe60e9b2e6ff170bfe6347
basamaryan <basam.aryan@gmail.com>
2022-08-28 12:00:07 +0200

    overlay: Import themed icons
    Author: Sonal Singh <sonal.singh.19993@gmail.com>
    Date:   Sat, 15 Jan 2022 21:05:06 +0000
    
        pixel: Add custom themed icons overlay
    
        Change-Id: Ia4901669b24793bcaa3c1ab9747782b2421e7c9c
        Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    
    Author: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Date:   Wed, 17 Aug 2022 02:13:21 +0530
    
        pixel: Update themed icons overlay
    
    Change-Id: I90b15b09f7a3d2fc29f699ef9084e8a423716de4

--- config/common.mk
+++ config/common.mk
@@ -243,7 +243,8 @@ PRODUCT_PACKAGE_OVERLAYS += \
 
 PRODUCT_PACKAGES += \
     NetworkStackOverlay \
-    TrebuchetOverlay
+    TrebuchetOverlay \
+    ThemedIconsOverlay
 
 # Translations
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/crowdin/overlay
